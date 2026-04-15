331ee9123b699e9f1dfb4fdb954998e2c7474936
Soumitra Chavan <schavan1@umd.edu>
2026-04-15 19:18:31 -0400

    Update weekly schedule recommendation tests

--- sage/tools shit/weekly_schedule_output.py
+++ sage/tools shit/weekly_schedule_output.py
@@ -30,6 +30,19 @@ def build_shift_eligibility_summary(employees: list[dict], employee_views: dict,
             else:
                 blocked_employees.append(employee_status)
 
+        # shift_summary.append(
+        #     {
+        #         "shift_id": shift["shift_id"],
+        #         "shift_date": shift["shift_date"],
+        #         "shift_start": shift["shift_start"],
+        #         "shift_end": shift["shift_end"],
+        #         "required_role": shift["required_role"],
+        #         "has_eligible_employee": len(eligible_employees) > 0,
+        #         "eligible_employees": eligible_employees,
+        #         "blocked_employees": blocked_employees,
+        #     }
+        # )
+
         recommended_employee_name = (
             eligible_employees[0]["employee_name"] if eligible_employees else None
         )
